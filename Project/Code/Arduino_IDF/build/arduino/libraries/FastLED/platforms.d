de865635cb4fc1e878c70cbb63444dd3dfc65665
Zander-M <sherlingford@yahoo.com>
2019-07-21 18:13:56 +0800

    clean build

--- Project/Code/Arduino_IDF/build/arduino/libraries/FastLED/platforms.d
+++
@@ -1,3 +0,0 @@
-libraries/FastLED//platforms.o: \
- /home/zdrm/Desktop/DURF/Project/Code/Arduino_IDF/components/arduino/libraries/FastLED/platforms.cpp
-/home/zdrm/Desktop/DURF/Project/Code/Arduino_IDF/components/arduino/libraries/FastLED//platforms.cpp:
--- Project/Code/Arduino_IDF/build/cxx/cxx_exception_stubs.d
+++
@@ -1,6 +0,0 @@
-cxx_exception_stubs.o: \
- /home/zdrm/esp/esp-idf/components/cxx/cxx_exception_stubs.cpp \
- /home/zdrm/Desktop/DURF/Project/Code/Arduino_IDF/build/include/sdkconfig.h
-
-/home/zdrm/Desktop/DURF/Project/Code/Arduino_IDF/build/include/sdkconfig.h:
-/home/zdrm/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
